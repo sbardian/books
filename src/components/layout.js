361049228f3d1b2d32782036a087b5ec59ae5dd4
Brian Andrews <sbardian@gmail.com>
2018-09-27 10:20:25 -0600

    Remove layout.css and other customizations

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,6 @@ import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 import '../../node_modules/tachyons/css/tachyons.min.css'
 
-// import Header from './header'
-import './layout.css'
 import './cssgrid.css'
 
 const Layout = ({ children }) => (
@@ -30,32 +28,19 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div> */}
+         */}
         <div className="container">
           <div className="header">
             <h1>{data.site.siteMetadata.title}</h1>
             <span>another</span>
           </div>
           <div className="content">
-            <div className="left-side">
-              <span>left side</span>
-            </div>
+            <div className="left-side" />
             <div className="main-content">
               <span>main content</span>
+              {children}
             </div>
-            <div className="right-side">
-              <span>right side</span>
-            </div>
+            <div className="right-side" />
           </div>
           <div className="footer">
             <span>link 1</span>
