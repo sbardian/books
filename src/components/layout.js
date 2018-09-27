f31a207e131b7705323d571b1b3b58fe47380145
Brian Andrews <sbardian@gmail.com>
2018-09-27 08:02:21 -0600

    Update site layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+// import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import '../../node_modules/tachyons/css/tachyons.min.css'
 
-import Header from './header'
+// import Header from './header'
 import './layout.css'
+import './cssgrid.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -19,7 +21,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Helmet
+        {/* <Helmet
           title={data.site.siteMetadata.title}
           meta={[
             { name: 'description', content: 'Sample' },
@@ -38,6 +40,27 @@ const Layout = ({ children }) => (
           }}
         >
           {children}
+        </div> */}
+        <div className="container">
+          <div className="header">
+            <h1>{data.site.siteMetadata.title}</h1>
+            <span>another</span>
+          </div>
+          <div className="content">
+            <div className="left-side">
+              <span>left side</span>
+            </div>
+            <div className="main-content">
+              <span>main content</span>
+            </div>
+            <div className="right-side">
+              <span>right side</span>
+            </div>
+          </div>
+          <div className="footer">
+            <span>link 1</span>
+            <span>link 2</span>
+          </div>
         </div>
       </>
     )}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+    <h1>Books I have read</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
