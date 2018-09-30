ba027e58e0bea36ec7ce2feeb8ad55faf6b44498
Brian Andrews <sbardian@gmail.com>
2018-09-29 23:57:53 -0600

    Implement search

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
@@ -14,71 +14,108 @@ a {
 }
 `;
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { children, onSearch } = this.props;
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
           }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'Books', content: 'Books I have read' },
-            { name: 'good books', content: 'good books' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <div
-          className={css`
-            display: grid;
-            grid-gap: 20px;
-            min-height: 100vh;
-            grid-template-rows: auto 1fr auto;
-            background: #606d80;
-          `}
-        >
-          <div
-            className={css`
-              display: grid;
-              grid-gap: 20px;
-              background: #1f1f20;
-              padding: 20px;
-            `}
-          >
-            <h1>{data.site.siteMetadata.title}</h1>
-          </div>
-          <div
-            className={css`
-              display: grid;
-              grid-gap: 20px;
-            `}
-          >
-            <div>{children}</div>
-          </div>
-          <div
-            className={css`
-              display: grid;
-              grid-gap: 20px;
-              grid-template-columns: 1fr;
-              justify-items: center;
-              align-items: center;
-              background: #1f1f20;
-              padding: 20px;
-            `}
-          />
-        </div>
-      </>
-    )}
-  />
-);
+        `}
+        render={data => (
+          <>
+            <Helmet
+              title={data.site.siteMetadata.title}
+              meta={[
+                { name: 'Books', content: 'Books I have read' },
+                { name: 'good books', content: 'good books' },
+              ]}
+            >
+              <html lang="en" />
+            </Helmet>
+            <div
+              className={css`
+                display: grid;
+                grid-gap: 20px;
+                min-height: 100vh;
+                grid-template-rows: auto 1fr auto;
+                background: #606d80;
+              `}
+            >
+              <div
+                className={css`
+                  display: grid;
+                  grid-gap: 20px;
+                  grid-template-columns: 1fr 400px;
+                  background: #1f1f20;
+                  align-items: center;
+                  padding: 20px;
+                `}
+              >
+                <h1>{data.site.siteMetadata.title}</h1>
+                <div
+                  className={css`
+                    display: grid;
+                    grid-gap: 20px;
+                    grid-template-columns: 1fr 400px;
+                    background: #1f1f20;
+                    align-items: center;
+                    padding: 20px;
+                  `}
+                >
+                  <input
+                    type="text"
+                    onChange={onSearch}
+                    className={css`
+                      font-size: 18pt;
+                      height: 40px;
+                    `}
+                  />
+                  <img
+                    className={css`
+                      height: 40px;
+                      width: 40px;
+                    `}
+                    src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-512.png"
+                  />
+                </div>
+              </div>
+              <div
+                className={css`
+                  display: grid;
+                  grid-gap: 20px;
+                `}
+              >
+                <div>{children}</div>
+              </div>
+              <div
+                className={css`
+                  display: grid;
+                  grid-gap: 20px;
+                  grid-template-columns: 1fr;
+                  justify-items: center;
+                  align-items: center;
+                  background: #1f1f20;
+                  padding: 20px;
+                `}
+              />
+            </div>
+          </>
+        )}
+      />
+    );
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { graphql } from 'gatsby';
 import { css } from 'react-emotion';
 import Layout from '../components/layout';
 import Book from '../components/Book';
 
-const IndexPage = ({
-  data: {
-    allBooksJson: { edges },
-  },
-}) => {
-  return (
-    <Layout>
-      <div
-        className={css`
-          display: grid;
-          grid-gap: 20px;
-          grid-template-columns: 1fr;
-          margin: 0 20px 0 20px;
-        `}
-      >
-        {edges.map(book => {
-          return <Book key={book.node.bookId} book={book.node} />;
-        })}
-      </div>
-    </Layout>
-  );
-};
+class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+    const {
+      data: {
+        allBooksJson: { edges },
+      },
+    } = this.props;
+    this.state = {
+      edges: edges,
+      originalEdges: edges,
+    };
+  }
+
+  handleSearch = ({ target }) => {
+    if (!target.value || target.value === '') {
+      this.setState({
+        edges: this.state.originalEdges,
+      });
+    } else {
+      const { originalEdges } = this.state;
+      this.setState({
+        edges: originalEdges.filter(book =>
+          book.node.title.toLowerCase().includes(target.value.toLowerCase())
+        ),
+      });
+    }
+  };
+
+  render() {
+    const { edges } = this.state;
+
+    return (
+      <Layout onSearch={this.handleSearch}>
+        <div
+          className={css`
+            display: grid;
+            grid-gap: 20px;
+            grid-template-columns: 1fr;
+            margin: 0 20px 0 20px;
+          `}
+        >
+          {edges.map(book => {
+            return <Book key={book.node.bookId} book={book.node} />;
+          })}
+        </div>
+      </Layout>
+    );
+  }
+}
 
 export default IndexPage;
 
