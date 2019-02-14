2705bcdf941ce94471a36812306ae5a9d15fe5b6
Brian Andrews <sbardian@gmail.com>
2019-02-14 08:30:42 -0700

    Update background and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,8 +11,8 @@ module.exports = {
         name: 'Sbardian Books',
         short_name: 'sbardian-books',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#606d80',
+        theme_color: '#606d80',
         display: 'minimal-ui',
         display: 'standalone',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
