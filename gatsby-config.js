b3b47ddee42d4d36c4e5f00c31d0469430842511
Brian Andrews <sbardian@gmail.com>
2019-02-13 20:57:05 -0700

    Update gatsby.config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Sbardian Books',
+        short_name: 'sbardian-books',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
+        display: 'standalone',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
