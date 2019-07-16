12ea867843d82468e1c7599bf3b0780f40fc0d34
Brian Andrews <sbardian@gmail.com>
2019-07-16 08:35:40 -0600

    refactor layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,52 @@
 /** @jsx jsx */
 // eslint-disable-next-line
 import React from "react"
-import { jsx, css, Global } from "@emotion/core"
 import PropTypes from "prop-types"
+import { jsx, css, Global } from "@emotion/core"
+import styled from "@emotion/styled"
 import Helmet from "react-helmet"
 import { graphql, useStaticQuery } from "gatsby"
 import { Breadcrumb } from "gatsby-plugin-breadcrumb"
-import Footer from './footer'
+import Footer from "./footer"
 import mq from "./media-queries"
 import "normalize.css"
+
+const PageContainer = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  min-height: 100vh;
+  grid-template-rows: auto 1fr auto;
+  background: #636080;
+`
+
+const PageTitleWrapper = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: 1fr;
+  background: #1f1f20;
+  align-items: center;
+  padding: 20px 0 20px 0;
+`
+
+const Title = styled.h1`
+  margin: 0.67em 200px;
+  ${mq.sm(css`
+    margin: 0 20px 0 20px;
+  `)};
+`
+
+const BreadcrumbWrapper = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: 1fr;
+  align-content: start;
+  margin: 0 200px 0 200px;
+  ${mq.xl(
+    css`
+      margin: 0 20px 0 20px;
+    `
+  )}
+`
 
 const Layout = ({ children, pageLocation, crumbLabel }) => {
   const {
@@ -46,33 +84,9 @@ const Layout = ({ children, pageLocation, crumbLabel }) => {
       >
         <html lang="en" />
       </Helmet>
-      <div
-        css={css`
-          display: grid;
-          grid-gap: 20px;
-          min-height: 100vh;
-          grid-template-rows: auto 1fr auto;
-          background: #636080;
-        `}
-      >
-        <div
-          css={css`
-            display: grid;
-            grid-gap: 20px;
-            grid-template-columns: 1fr;
-            background: #1f1f20;
-            align-items: center;
-            padding: 20px 0 20px 0;
-          `}
-        >
-          <h1
-            css={css`
-              margin: 0.67em 200px;
-              ${mq.sm(css`
-                margin: 0 20px 0 20px;
-              `)};
-            `}
-          >
+      <PageContainer>
+        <PageTitleWrapper>
+          <Title>
             <a
               css={css`
                 text-decoration: none;
@@ -81,34 +95,14 @@ const Layout = ({ children, pageLocation, crumbLabel }) => {
             >
               {title}
             </a>
-          </h1>
-        </div>
-        <div
-          css={css`
-            display: grid;
-            grid-gap: 20px;
-          `}
-        >
-          <div
-            css={css`
-              display: grid;
-              grid-gap: 20px;
-              grid-template-columns: 1fr;
-              align-content: start;
-              margin: 0 200px 0 200px;
-              ${mq.xl(
-                css`
-                  margin: 0 20px 0 20px;
-                `
-              )}
-            `}
-          >
-            <Breadcrumb location={pageLocation} crumbLabel={crumbLabel} />
-            {children}
-          </div>
-        </div>
+          </Title>
+        </PageTitleWrapper>
+        <BreadcrumbWrapper>
+          <Breadcrumb location={pageLocation} crumbLabel={crumbLabel} />
+          {children}
+        </BreadcrumbWrapper>
         <Footer />
-      </div>
+      </PageContainer>
     </>
   )
 }
