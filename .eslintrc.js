cc743ac52ac35dcd11a547232dadb3f769d40541
Brian Andrews <sbardian@gmail.com>
2019-09-04 09:16:50 -0600

    chore(eslint): move to js config file

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ["airbnb", "prettier", "prettier/react"],
+  plugins: ["emotion", "prettier"],
+  env: {
+    jest: true,
+  },
+  parser: "babel-eslint",
+  rules: {
+    "react/jsx-filename-extension": "off",
+  },
+}
