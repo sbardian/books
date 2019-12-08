b7f800cdf42deaf6c0c6ce29563dd68210372110
Brian Andrews <sbardian@gmail.com>
2019-12-08 16:28:04 -0700

    test: update footer test

--- src/components/footer.test.js
+++ src/components/footer.test.js
@@ -2,8 +2,10 @@ import React from "react"
 import { render } from "@testing-library/react"
 import Footer from "./footer"
 
-test("footer success test", () => {
-  const { getByTestId } = render(<Footer />)
-  const link = getByTestId("footer-link-test")
-  expect(link).toHaveAttribute("href", "https://gatsbyjs.org")
+describe("Footer test", () => {
+  it("should return footer link", () => {
+    const { getByTestId } = render(<Footer />)
+    const link = getByTestId("footer-link-test")
+    expect(link).toHaveAttribute("href", "https://gatsbyjs.org")
+  })
 })
