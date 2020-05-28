6e2e28fdcf8d2931a5a9ff1c25060d6203648815
Brian Andrews <sbardian@gmail.com>
2020-05-28 13:33:34 -0600

    chore: remove old desktop first media queries

--- src/components/media-queries.js
+++ src/components/media-queries.js
@@ -1,29 +1,3 @@
-// // Adapted from https://emotion.sh/docs/media-queries
-// /** @jsx jsx */
-// import { css } from "@emotion/core"
-
-// const breakpoints = {
-//   // mobile-first, so there is no 'xs' for portrait phones
-//   sm: 576, // landscape phones
-//   md: 768, // tablets
-//   lg: 992, // landscape tablets and desktops
-//   xl: 1275, // extra large desktops
-// }
-
-// const mq = Object.keys(breakpoints).reduce((accumulator, label) => {
-//   const prefix = typeof breakpoints[label] === "string" ? "" : "max-width:"
-//   const suffix = typeof breakpoints[label] === "string" ? "" : "px"
-//   accumulator[label] = cls =>
-//     css`
-//       @media (${prefix + breakpoints[label] + suffix}) {
-//         ${cls};
-//       }
-//     `
-//   return accumulator
-// }, {})
-
-// export default mq
-
 // Source: https://github.com/wKovacs64/pwl/blob/bd79791b2fa54c14de186b64203f9350a9bea986/src/utils/mq.ts
 
 const breakpointMap = {
