80ac7df90f7bfec5c052266128d91d52fc55552d
Brian Andrews <sbardian@gmail.com>
2020-06-22 21:59:29 -0600

    chore: add git add back to lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "*.{js,ts,tsx}": ["prettier --write", "eslint --fix"],
-  "*.{html,json,md,yml,yaml}": ["prettier --write"],
+  "*.{js,ts,tsx}": ["prettier --write", "eslint --fix", "git add"],
+  "*.{html,json,md,yml,yaml}": ["prettier --write", "git add"],
 }
